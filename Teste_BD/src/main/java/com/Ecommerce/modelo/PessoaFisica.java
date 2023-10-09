77aab6e5e8cac9f408969ee9603952a6aa887de9
joao pedro <joao.ferreira01br@gmail.com>
2023-10-09 19:30:10 -0300

    MY SQL SERVER MANAGENT (FIX)

--- Teste_BD/src/main/java/com/Ecommerce/modelo/PessoaFisica.java
+++ Teste_BD/src/main/java/com/Ecommerce/modelo/PessoaFisica.java
@@ -1,89 +1,89 @@
-package com.Ecommerce.modelo;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import lombok.Data;
-
-@Data
-@Entity
-public class PessoaFisica {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	//Tela de cadastro pessoa fisica
-
-	private String nomeCompleto;
-
-	private String cpf;
-
-	private String email;
-
-	private String telefone;
-
-	private String senha;
+	package com.Ecommerce.modelo;
 	
-	//role
-	private String role;
-
-
-	public String getRole() {
-		return role;
+	import javax.persistence.Entity;
+	import javax.persistence.GeneratedValue;
+	import javax.persistence.GenerationType;
+	import javax.persistence.Id;
+	
+	import lombok.Data;
+	
+	@Data
+	@Entity
+	public class PessoaFisica {
+	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+		//Tela de cadastro pessoa fisica
+	
+		private String nomeCompleto;
+	
+		private String cpf;
+	
+		private String email;
+	
+		private String telefone;
+	
+		private String senha;
+		
+		//role
+		private String role;
+	
+	
+		public String getRole() {
+			return role;
+		}
+	
+		public void setRole(String role) {
+			this.role = role;
+		}
+	
+		public Long getId() {
+			return id;
+		}
+	
+		public void setId(Long id) {
+			this.id = id;
+		}
+	
+		public String getNomeCompleto() {
+			return nomeCompleto;
+		}
+	
+		public void setNomeCompleto(String nomeCompleto) {
+			this.nomeCompleto = nomeCompleto;
+		}
+	
+		public String getCpf() {
+			return cpf;
+		}
+	
+		public void setCpf(String cpf) {
+			this.cpf = cpf;
+		}
+	
+		public String getEmail() {
+			return email;
+		}
+	
+		public void setEmail(String email) {
+			this.email = email;
+		}
+	
+		public String getTelefone() {
+			return telefone;
+		}
+	
+		public void setTelefone(String telefone) {
+			this.telefone = telefone;
+		}
+	
+		public String getSenha() {
+			return senha;
+		}
+	
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+	
 	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNomeCompleto() {
-		return nomeCompleto;
-	}
-
-	public void setNomeCompleto(String nomeCompleto) {
-		this.nomeCompleto = nomeCompleto;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-}
